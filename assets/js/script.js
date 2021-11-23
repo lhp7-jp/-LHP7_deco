8fed5ff0e6d8e5b99040f6de3fc14d8541cdf781
AlexyL <alexy.lepretre76@laposte.net>
2021-11-23 16:24:58 +0100

    Fonctionnalité incrémentation produit dans panier

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,60 +11,71 @@ if (e.target.nodeName == "A") {
     fetch("assets/json/produits.json")
         .then(response => response.json())
         .then(data => {
+
             listProducts.innerHTML = ""
-                    data.products.forEach(element => {
-                        if (e.target.dataset.category == element.category) {
-                        listProducts.innerHTML += `
-                    <div class="col-lg-4 text-center card-body">
 
-                        <img id="picture1" src="assets/img/${element.img}" data-show class="rounded mx-auto d-block img-fluid img-thumbnail" alt="cards-images">
-                        <h5 id="title1" class="card-title" >${element.name}</h5>
-                        <p id="description1" class="card-text" >${element.description}</p>
-                        <a id="price1" href="#" class="btn btn-secondary m-2" >${element.price}€</a>
-                        <button type="button" class="btn btn-primary addPanier" id="${element.id}">Ajout au panier</button>
+            data.products.forEach(element => {
 
-                    </div>
-                        `
-                    }
-                })
-                // ici commence l'écoute sur les boutons ajout au panier
-                let btnAddPanier = Array.from(document.getElementsByClassName("addPanier"));
-                btnAddPanier.forEach(element => {
-                    element.addEventListener("click", e => {
-                        if (!arrayPanier.includes(e.target.id)) {
-                        arrayPanier.push(e.target.id);
-                        // console.log(arrayPanier);
+                if (e.target.dataset.category == element.category) {
+                listProducts.innerHTML += `
+                                        <div class="col-lg-4 text-center card-body">
+
+                                            <img id="picture1" src="assets/img/${element.img}" data-show class="rounded mx-auto d-block img-fluid img-thumbnail" alt="cards-images">
+                                            <h5 id="title1" class="card-title" >${element.name}</h5>
+                                            <p id="description1" class="card-text" >${element.description}</p>
+                                            <a id="price1" href="#" class="btn btn-secondary m-2" >${element.price}€</a>
+                                            <button type="button" class="btn btn-primary addPanier" id="${element.id}">Ajout au panier</button>
+
+                                        </div>
+                `
+                }
+            })
+
+            Array.from(document.getElementsByClassName("addPanier")).forEach(element => {
+
+                element.addEventListener("click", e => {
+
+                    if (!arrayPanier.includes(e.target.id)) {
+
+                        arrayPanier.push(e.target.id)
+
                         fetch("assets/json/produits.json")
-                        .then(response => response.json())
-                        .then(data => {
-                         data.products.forEach(element => {
+                            .then(response => response.json())
+                            .then(data => {
+                            data.products.forEach(element => {
                             
-                             if (element.id == e.target.id){
-                                contentModal.innerHTML += ` ${element.name}`
-                                testId = e.target.id;
-                                var cptProduct = 1;
-                            }
-                         });  
+                                if (element.id == e.target.id){
+                                    contentModal.innerHTML += `
+                                                            <tr>
+                                                                <th scope="row">${element.id}</th>
+                                                                <td>${element.name}</td>
+                                                                <td><input id="qte-${element.id}" type="number" placeholder="" min="0" max="20" value="1"></td>
+                                                                <td>${element.price}</td>
+                                                                <td><a href=""><i class="fas fa-times text-dark"></i></a></td>
+                                                            </tr>
+                                                            `
+                                }
+                            })
                         })  
-                        }
-                        else {
-                            console.log(testId)
-                            if (testId == e.target.id) {
-                            cptProduct++;
-                            console.log(cptProduct);
-                            }
-                            else {
-                                 console.log(cptProduct);
-                            }
-                            
-                        }
+                    }
+                    else {
+                            addQte(`qte-${e.target.id}`)
                         
-                    })
+                    }
+                    
                 })
-              
+            })
+                
                 
             })
         }
     })
+
+function addQte(id){
+
+    let ouioui = document.getElementById(id).value++
+    console.log(ouioui)
+
+}
 
 
