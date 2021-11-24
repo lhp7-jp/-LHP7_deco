2f2ba0db5887ebb19bf94fddc90057660992128d
AlexyL <alexy.lepretre76@laposte.net>
2021-11-24 16:34:41 +0100

    ajout du total et suppression ligne du panier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-const category = document.getElementById("category");
-const listProducts = document.getElementById("listProducts");
-const contentModal = document.getElementById("contentModal");
-let fisrtConnect = true
-let arrayPanier = [];
+const category = document.getElementById("category")
+const listProducts = document.getElementById("listProducts")
+const contentModal = document.getElementById("contentModal")
+const tablePrice = document.getElementById("tablePrice")
+let arrayPanier = []
 
 category.addEventListener("click", e => {
 
@@ -11,9 +11,9 @@ category.addEventListener("click", e => {
     if (e.target.nodeName == "A") {
 
         document.querySelectorAll("[data-category]").forEach(element => {
-            if(element.dataset.category == e.target.dataset.category){
+            if (element.dataset.category == e.target.dataset.category) {
                 element.classList.add("activeListNav")
-            }else{
+            } else {
                 element.classList.remove("activeListNav")
             }
         })
@@ -60,29 +60,56 @@ category.addEventListener("click", e => {
                                         if (element.id == e.target.id) {
 
                                             let tr = document.createElement('tr')
+                                            tr.setAttribute("id", `line-${element.id}`)
                                             tr.innerHTML = `
                                                         <th scope="row">${element.id}</th>
                                                         <td>${element.name}</td>
+                                                        <td>${element.price} €</td>
                                                         <td><input id="qte-${element.id}" type="number" placeholder="" min="0" max="20" data-priceunit="${element.price}" data-qte="1" value="1" style="width: 42px;"></td>
-                                                        <td id="price-${element.id}">${element.price} €</td>
-                                                        <td><a href=""><i class="text-primary fas fa-times"></i></a></td>
+                                                        <td id="price-${element.id}" data-priceqte="${element.price}" data-total>${element.price} €</td>
+                                                        <td><i class="text-primary fas fa-times" data-idtr="line-${element.id}""></i></td>
                                                     `
                                             contentModal.appendChild(tr)
 
+                                            totalCaddie()
                                         }
                                     })
                                 })
-
                         } else {
-
-
-
+                            let qty = document.getElementById(`qte-${e.target.id}`).dataset.qte++ + 1
+                            let priceCurrent = document.getElementById(`qte-${e.target.id}`).dataset.priceunit
+                            document.getElementById(`qte-${e.target.id}`).value = qty
+                            document.getElementById(`price-${e.target.id}`).textContent = Number(priceCurrent * qty).toFixed(2) + " €"
+                            document.getElementById(`price-${e.target.id}`).dataset.priceqte = Number(priceCurrent * qty).toFixed(2)
+                            totalCaddie()
                         }
-
                     })
                 })
-
-
             })
     }
 })
+
+tablePrice.addEventListener("change", e => {
+    if (e.target.tagName == "INPUT") {
+        e.target.dataset.qte = e.target.value
+        document.getElementById(`price-${e.target.id.split("-").pop()}`).textContent = Number(e.target.dataset.priceunit * e.target.value).toFixed(2) + " €"
+        document.getElementById(`price-${e.target.id.split("-").pop()}`).dataset.priceqte = Number(e.target.dataset.priceunit * e.target.value).toFixed(2)
+        totalCaddie()
+    }
+})
+
+tablePrice.addEventListener("click", e => {
+    if(e.target.nodeName == "I"){
+        let childRemove = document.getElementById(e.target.dataset.idtr)
+        contentModal.removeChild(childRemove)
+        totalCaddie()
+    }
+})
+
+function totalCaddie() {
+    let totalCaddie = 0
+    document.querySelectorAll("[data-total]").forEach(elt => {
+        totalCaddie += Number(elt.dataset.priceqte)
+    })
+    total.textContent = totalCaddie
+}
