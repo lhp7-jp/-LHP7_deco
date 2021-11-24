503ee6d19613163f4d02b681e78b21f2659e4177
AlexyL <alexy.lepretre76@laposte.net>
2021-11-24 09:24:12 +0100

    correction mineur bug sur insertion element au panier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,88 @@
 const category = document.getElementById("category");
 const listProducts = document.getElementById("listProducts");
 const contentModal = document.getElementById("contentModal");
-var testId = "";
-var cptProduct = 1;
+let fisrtConnect = true
 let arrayPanier = [];
 
 category.addEventListener("click", e => {
-e.preventDefault()
-if (e.target.nodeName == "A") {
-    fetch("assets/json/produits.json")
-        .then(response => response.json())
-        .then(data => {
 
-            listProducts.innerHTML = ""
+    e.preventDefault()
 
-            data.products.forEach(element => {
+    if (e.target.nodeName == "A") {
 
-                if (e.target.dataset.category == element.category) {
-                listProducts.innerHTML += `
+        document.querySelectorAll("[data-category]").forEach(element => {
+            if(element.dataset.category == e.target.dataset.category){
+                element.classList.add("activeListNav")
+            }else{
+                element.classList.remove("activeListNav")
+            }
+        })
+
+        arianeCurrent.textContent = e.target.dataset.category
+        titleCurrentPage.textContent = e.target.dataset.category
+
+        fetch("assets/json/produits.json")
+            .then(response => response.json())
+            .then(data => {
+
+                listProducts.innerHTML = ""
+
+                data.products.forEach(element => {
+
+                    if (e.target.dataset.category == element.category) {
+                        listProducts.innerHTML += `
                                         <div class="col-lg-4 text-center card-body">
 
                                             <img id="picture1" src="assets/img/${element.img}" data-show class="rounded mx-auto d-block img-fluid img-thumbnail" alt="cards-images">
                                             <h5 id="title1" class="card-title" >${element.name}</h5>
                                             <p id="description1" class="card-text" >${element.description}</p>
-                                            <a id="price1" href="#" class="btn btn-secondary m-2" >${element.price}€</a>
-                                            <button type="button" class="btn btn-primary addPanier" id="${element.id}">Ajout au panier</button>
+                                            <a id="price1" href="#" class="btn btn-outline-success m-2" >${element.price}€</a>
+                                            <button type="button" class="btn btn-primary" data-addtocaddie id="${element.id}">Ajout au panier</button>
 
                                         </div>
                 `
-                }
-            })
-
-            Array.from(document.getElementsByClassName("addPanier")).forEach(element => {
-
-                element.addEventListener("click", e => {
-
-                    if (!arrayPanier.includes(e.target.id)) {
-
-                        arrayPanier.push(e.target.id)
-
-                        fetch("assets/json/produits.json")
-                            .then(response => response.json())
-                            .then(data => {
-                            data.products.forEach(element => {
-                            
-                                if (element.id == e.target.id){
-                                    contentModal.innerHTML += `
-                                                            <tr>
-                                                                <th scope="row">${element.id}</th>
-                                                                <td>${element.name}</td>
-                                                                <td><input id="qte-${element.id}" type="number" placeholder="" min="0" max="20" value="1"></td>
-                                                                <td>${element.price}</td>
-                                                                <td><a href=""><i class="fas fa-times text-dark"></i></a></td>
-                                                            </tr>
-                                                            `
-                                }
-                            })
-                        })  
                     }
-                    else {
-                            addQte(`qte-${e.target.id}`)
-                        
-                    }
-                    
                 })
+
+                document.querySelectorAll("[data-addtocaddie]").forEach(element => {
+
+                    element.addEventListener("click", e => {
+
+                        if (!arrayPanier.includes(e.target.id)) {
+
+                            arrayPanier.push(e.target.id)
+
+                            fetch("assets/json/produits.json")
+                                .then(response => response.json())
+                                .then(data => {
+                                    data.products.forEach(element => {
+
+                                        if (element.id == e.target.id) {
+
+                                            let tr = document.createElement('tr')
+                                            tr.innerHTML = `
+                                                        <th scope="row">${element.id}</th>
+                                                        <td>${element.name}</td>
+                                                        <td><input id="qte-${element.id}" type="number" placeholder="" min="0" max="20" data-priceunit="${element.price}" data-qte="1" value="1" style="width: 42px;"></td>
+                                                        <td id="price-${element.id}">${element.price} €</td>
+                                                        <td><a href=""><i class="text-primary fas fa-times"></i></a></td>
+                                                    `
+                                            contentModal.appendChild(tr)
+
+                                        }
+                                    })
+                                })
+
+                        } else {
+
+
+
+                        }
+
+                    })
+                })
+
+
             })
-                
-                
-            })
-        }
-    })
-
-function addQte(id){
-
-    let ouioui = document.getElementById(id).value++
-    console.log(ouioui)
-
-}
-
-
+    }
+})
